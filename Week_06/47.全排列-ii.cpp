84bf6e403ad8110c11db96683ef0789cc4493850
kangWork <1326799350@qq.com>
2020-09-21 08:49:53 +0800

    20200921

--- Week_06/47.全排列-ii.cpp
+++ Week_06/47.全排列-ii.cpp
@@ -7,8 +7,33 @@
 // @lc code=start
 class Solution {
 public:
+    vector<int> visited;
+    void backtrack(vector<vector<int>> &res, vector<int>& nums, vector<int>& path, int index)
+    {
+        if(nums.size() == index)
+        {
+            res.push_back(path);
+            return;
+        }
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(visited[i] || (i > 0 && nums[i-1] == nums[i] && !visited[i-1]))
+                continue;
+            path.push_back(nums[i]);
+            visited[i] = 1;
+            backtrack(res, nums, path, index+1);
+            visited[i] = 0;
+            path.pop_back();
+        }
+    }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        
+        vector<vector<int>> res;
+        vector<int> path;
+        visited.resize(nums.size());
+        sort(nums.begin(),nums.end());
+
+        backtrack(res, nums, path, 0);
+        return res;    
     }
 };
 // @lc code=end
